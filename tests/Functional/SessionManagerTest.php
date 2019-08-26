f660585f7d1adeac8f6e0c2448606b432068072f
Andrew Do <andrewdough@gmail.com>
2019-08-26 16:28:17 -0700

    add functional tests

--- tests/Functional/SessionManagerTest.php
+++ tests/Functional/SessionManagerTest.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use DoubleRatchet\Exceptions\DecryptionFailedException;
 use DoubleRatchet\KeyPair;
+use DoubleRatchet\PrivateKey;
 use DoubleRatchet\SessionManager;
 use PHPUnit\Framework\TestCase;
 use Faker\Generator;
@@ -15,6 +17,10 @@ final class SessionManagerTest extends TestCase
     private $serverSessionManager;
     /** @var SessionManager */
     private $clientSessionManager;
+    /** @var KeyPair */
+    private $serverIdentity;
+    /** @var KeyPair */
+    private $clientIdentity;
     /** @var Generator */
     private $faker;
 
@@ -25,24 +31,56 @@ final class SessionManagerTest extends TestCase
     {
         parent::setUp();
 
-        $serverIdentity = KeyPair::getNewKeyPair();
-        $clientIdentity = KeyPair::getNewKeyPair();
+        $this->serverIdentity = KeyPair::getNewKeyPair();
+        $this->clientIdentity = KeyPair::getNewKeyPair();
         $preKey = KeyPair::getNewKeyPair();
 
         $this->serverSessionManager = new SessionManager(
-            $serverIdentity->getPrivateKey(),
-            $clientIdentity->getPublicKey(),
+            $this->serverIdentity->getPrivateKey(),
+            $this->clientIdentity->getPublicKey(),
             $preKey->getPublicKey(),
             $preKey->getPrivateKey()
         );
 
         $this->clientSessionManager = new SessionManager(
-            $clientIdentity->getPrivateKey(),
-            $serverIdentity->getPublicKey(),
+            $this->clientIdentity->getPrivateKey(),
+            $this->serverIdentity->getPublicKey(),
             $preKey->getPublicKey()
         );
 
         $this->faker = Factory::create();
+    }
+
+    public function testDecryptMessageRequiresEncryptedStringIVAndHmac()
+    {
+        self::expectException(DecryptionFailedException::class);
+        self::expectExceptionMessage('Message must include encrypted string, IV and HMAC values');
+
+        $this->serverSessionManager->decryptMessage('missing everything');
+
+        self::expectException(DecryptionFailedException::class);
+        self::expectExceptionMessage('Message must include encrypted string, IV and HMAC values');
+
+        $this->serverSessionManager->decryptMessage('some_encrypted_string:some_iv');
+    }
+
+    public function testHandlesInvalidMessage()
+    {
+        $badSecret = new PrivateKey(str_pad('', 32, "\0"));
+        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
+        $invalidMessage = openssl_encrypt(
+            $this->faker->sentence,
+            'aes-256-cbc',
+            $badSecret->getValue(),
+            0,
+            $iv
+        );
+        $invalidMessage = $invalidMessage . ':' . base64_encode($iv);
+        $invalidMessage = $invalidMessage . ':' . base64_encode(hash_hmac('sha256', $invalidMessage, $badSecret->getValue()));
+        self::expectException(DecryptionFailedException::class);
+        self::expectExceptionMessage('Unable to decrypt message');
+
+        $this->serverSessionManager->decryptMessage($invalidMessage);
     }
 
     public function testSerialRequestResponses()
@@ -67,5 +105,16 @@ final class SessionManagerTest extends TestCase
             unset($decryptedData->ratchet_key);
             self::assertEquals($responseData, (array)$decryptedData);
         }
+    }
+
+    public function testGetAsEncryptedString()
+    {
+        $sessionManager = $this->serverSessionManager;
+        $sessionManagerString = $this->serverSessionManager->getAsEncryptedString();
+
+        self::assertEquals(
+            $sessionManager,
+            SessionManager::getFromEncryptedString($this->serverIdentity->getPrivateKey(), $sessionManagerString)
+        );
     }
 }
